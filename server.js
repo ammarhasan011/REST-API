77259eb23d5832e4d49114906e82ff905fed848d
Ammar Hasan <ammarhasan011@gmail.com>
2022-12-20 16:22:03 +0100

    a

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+//const color = require('color');
 const app = express();
 
-app.listen(3000, () => console.log('Your innnn!'));
 
+app.listen(3000, () => console.log('Server is up, your innnn!'));
 
+// app.get('/api/products', () => { })
 
+app.get('/', function (req, res) { res.send('Helloo worldd123') });
