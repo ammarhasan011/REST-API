3635b35076e1b95c1e71353f1e5a6533c48d34bb
Ammar Hasan <ammarhasan011@gmail.com>
2022-12-21 22:56:19 +0100

    a

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
-//const color = require('color');
 const app = express();
 const port = 3000
 
-app.get('/', function (req, res) { res.send('Helloo worldd') });
-app.get('/api/products', () => { })
+const products = require('./product.json');
+console.log(products);
+
+app.get('/', function (req, res) { res.send('Helo World! Personal') });
+
+
+app.get('/api/products/:id', (req, res) => {
+    const user = product.find(p => p.id === (req.params.id))
+    if (!user)
+        res.status(404).send('Not Found!')
+    res.send(user)
+});
+
+app.get('/api/products', function (req, res) { res.send('Helloo worlddd12') });
+
 
 app.post(() => { })
 
-app.listen(port, () => console.log('Server is up, your innnn!'));
 
-// get
-// delete
-// put
-// post
+// res.status(200).send('Här är producten med id' + req.params.id)
+
+
+
+
+app.listen(port, () => console.log('Server is up, your innnn!'));
