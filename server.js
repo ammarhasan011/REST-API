b78d2447c91fa4ff0cc3e2046085287e14799904
Ammar Hasan <ammarhasan011@gmail.com>
2022-12-23 05:00:51 +0100

    a

--- server.js
+++ server.js
@@ -1,27 +1,83 @@
+//Rest API, skapar med express
 const express = require('express');
 const app = express();
 const port = 3000
 
+const newid = [
+
+    {
+        "id": 5,
+        "name": "Molgan",
+        "lastname": "Johansson",
+        "age": "20",
+        "längd": "120"
+    }
+];
+
+//medelande
+app.get('/', (req, res) => { res.send('Hello Hello') });
+
+//kopplar till min product.json
 const products = require('./product.json');
 console.log(products);
 
-app.get('/', function (req, res) { res.send('Helo World! Personal') });
+
+//visar alla mina id:s
+app.get('/api/products/', (req, res) => {
+    res.status(200).send(products)
+});
 
 
+//Hämtar en id från listan
 app.get('/api/products/:id', (req, res) => {
-    const user = product.find(p => p.id === (req.params.id))
+    const user = products.find(p => p.id == (req.params.id))
     if (!user)
-        res.status(404).send('Not Found!')
+        res.status(400).send('Not Found!')
     res.send(user)
 });
 
-app.get('/api/products', function (req, res) { res.send('Helloo worlddd12') });
 
 
-app.post(() => { })
+
+
+
+
+
+app.post('/api/products/', (req, res) => {
+    const postid = {
+        id: products.length + 1,
+        name: req.body.name
+    };
+    products.push(postid);
+    res.send(postid)
+})
+// const postid = require('newid');
+// if (!postid)
+//     res.status(404).send('fan oxå')
+
+
 
 
 // res.status(200).send('Här är producten med id' + req.params.id)
+//
+app.delete('//api/product/:id', (req, res) => {
+    const deleteid = products.find(p => p.id == (req.params.id))
+    if (deleteid)
+        products.indexOf(deleteid)
+    res.status(302).send('deleteid')
+    //else
+    // products.indexOf(deleteid)
+    res.status(404).send('Fail')
+})
+
+// app.PUT('//api/product/:id', (req, res) => {
+//    const putid= products
+//     res.send('hej')
+// })
+
+
+
+
 
 
 
