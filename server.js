c29450cbf431e8e9a1fdce50179d4ac743d8b50e
Ammar Hasan <ammarhasan011@gmail.com>
2023-01-06 21:05:27 +0100

    a

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const fs = require("fs");
 const app = express();
 const port = 3000
+const cors = require('cors')
+
 app.use(express.json());
+app.use(cors());
 
 
 
